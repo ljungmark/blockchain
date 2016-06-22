326431031a0155d8ab19fc069e802bd2a245ac22
ljungmark <mattias@ljungmark.co>
2016-06-22 19:24:30 +0200

    Add new block func based on last block index

--- blockchain.js
+++ blockchain.js
@@ -9,4 +9,14 @@ class Blockchain {
         const genesisDate = '2/15/1987';
         return new Block('Genesis Block', 0, genesisDate);
     }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addNewBlock(newBlock) {
+        newBlock.index = this.getLastBlock().index + 1;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
 }
