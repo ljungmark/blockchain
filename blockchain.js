45fdd58fef8f83ba1dc312704cafa55ff28744a0
ljungmark <mattias@ljungmark.co>
2016-06-22 19:22:24 +0200

    Genesis Block

--- blockchain.js
+++ blockchain.js
@@ -1,1 +1,12 @@
 const Block = require('./block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        const genesisDate = '2/15/1987';
+        return new Block('Genesis Block', 0, genesisDate);
+    }
+}
